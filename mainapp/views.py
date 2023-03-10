a6db600e0972caace70fde739118de0957e73c04
jshuckbot <93859862+jshuckbot@users.noreply.github.com>
2023-03-10 16:24:22 +0700

    Lesson 2 (#1)
    * lesson_2 Lesson 2 ready

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class MainPageView(TemplateView):
+    template_name = "mainapp/index.html"
+
+
+class NewsPageView(TemplateView):
+    template_name = "mainapp/news.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["news_title"] = "Громкий новостной заголовок"
+        context["news_preview"] = "Предворительное описание, которое заинтересует каждого"
+
+        return context
+
+
+class CoursesPageView(TemplateView):
+    template_name = "mainapp/courses_list.html"
+
+
+class ContactsPageView(TemplateView):
+    template_name = "mainapp/contacts.html"
+
+
+class DocSitePageView(TemplateView):
+    template_name = "mainapp/doc_site.html"
+
+
+class LoginPageView(TemplateView):
+    template_name = "mainapp/login.html"
